6e19c3d23a50595b60e49cc10b4b93e079dc6b56
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-09-09 00:14:40 +0900

    kaminariのインストール、ページネーションの実装
    ページネーションを実装する上で必要となるkaminariというgemのインストールと、ページネーションの実装。
    configファイルの編集により、1ページに表示する件数を変更することもできる。

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
-    @boards = Board.all
+    # @boards = Board.all
+    @boards = Board.page(params[:page])
   end
 
   def new
